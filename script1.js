a5accdd376ce7926c0debb03383c8208cd20670b
Sadique Hasan <sadiquehasan03@gmail.com>
2023-12-25 22:59:43 +0530

    login page added

---
+++ script1.js
@@ -0,0 +1,13 @@
+function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    if (username === 'Sadique' && password === '12345') {
+      // Redirect to main.html if login is successful
+      window.location.href = 'main.html';
+    } else {
+      // Display an alert for unsuccessful login
+      alert('Invalid username or password');
+    }
+  }
+  
