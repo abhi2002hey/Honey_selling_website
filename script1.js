da8cf4c1b17940ee41a8f3606042e73ce6600a4f
abhi24112 <onlystudy24112002@gmail.com>
2023-12-26 13:15:06 +0530

    Update login functionality

--- script1.js
+++ script1.js
@@ -1,4 +1,21 @@
-function login() {
+// function login() {
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
+  
+//     if (username === 'admin' && password === '12345') {
+//       // Redirect to main.html if login is successful
+//       window.location.href = 'main.html';
+//     } else {
+//       // Display an alert for unsuccessful login
+//       alert('Invalid username or password');
+//     }
+//   }
+  
+
+// New code 
+  function login(event) {
+    event.preventDefault();
+    // Rest of the function
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
   
@@ -10,4 +27,3 @@ function login() {
       alert('Invalid username or password');
     }
   }
-  
